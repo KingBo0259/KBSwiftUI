8c686ef4aba204e9fc3bfb83d8f5b768250ba305
金灵波 <lingbo.jin@rokid.com>
2019-06-19 17:09:16 +0800

    [ADD] add presentbutton

---
+++ KBSwiftUI/KBSwiftUI/Study/View&Controls/Presentations/KBPresentView.swift
@@ -0,0 +1,29 @@
+//
+//  KBPresentView.swift
+//  KBSwiftUI
+//
+//  Created by 金灵波 on 2019/6/19.
+//  Copyright © 2019 金灵波. All rights reserved.
+//
+
+import SwiftUI
+
+struct KBPresentViewDetail:View {
+    var body: some View {
+        Text("KB present View")
+    }
+}
+
+struct KBPresentView : View {
+    var body: some View {
+        PresentationButton(Text("Click to Present View"), destination: KBPresentViewDetail())
+     }
+}
+
+#if DEBUG
+struct KBPresentView_Previews : PreviewProvider {
+    static var previews: some View {
+        KBPresentView()
+    }
+}
+#endif
