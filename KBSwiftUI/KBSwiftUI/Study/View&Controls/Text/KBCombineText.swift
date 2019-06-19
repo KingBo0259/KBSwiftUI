6cb78a0aaf7f467bf4f7b920a3a984d4d81e953d
金灵波 <lingbo.jin@rokid.com>
2019-06-19 20:48:22 +0800

    [ADD] add KBCombineText

---
+++ KBSwiftUI/KBSwiftUI/Study/View&Controls/Text/KBCombineText.swift
@@ -0,0 +1,47 @@
+
+//
+//  KBCombineText.swift
+//  KBSwiftUI
+// https://www.hackingwithswift.com/quick-start/swiftui/how-to-combine-text-views-together
+//  Created by 金灵波 on 2019/6/19.
+//  Copyright © 2019 金灵波. All rights reserved.
+//
+
+import SwiftUI
+
+struct KBCombineText : View {
+    var note = """
+
+        Not allow:
+        Text("SwiftUI ")
+            .foregroundColor(.red)
+        + Text("is ")
+            .foregroundColor(.orange)
+        + Text("awesome".foregroundColor(.blue)
+        Warning: Some modifiers change a property of your view, whereas others return a different, modified view, which can cause unhelpful compiler errors. For example, this is not allowed:
+
+        """
+    var body: some View {
+        VStack{
+            
+            
+            Text("SwiftUI").font(.largeTitle).color(.yellow
+                )
+                + Text(" is ").color(.blue)
+                    .font(.headline)
+                + Text("awesome").font(.footnote).bold()
+            
+            Divider()
+            Text(note).lineLimit(0)
+        }
+      
+    }
+}
+
+#if DEBUG
+struct KBCombineText_Previews : PreviewProvider {
+    static var previews: some View {
+        KBCombineText()
+    }
+}
+#endif
