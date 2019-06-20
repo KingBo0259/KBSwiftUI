c9d48c7ffcd866c97ff3117e22c9c573b7913c00
金灵波 <lingbo.jin@rokid.com>
2019-06-20 13:21:15 +0800

    [MDF] update

--- KBSwiftUI/KBSwiftUI/ContentView.swift
+++ KBSwiftUI/KBSwiftUI/ContentView.swift
@@ -10,62 +10,76 @@ import SwiftUI
 
 struct ContentView : View {
     @State var enableLogging = false
-    
     @State var selectedColor = 0
     @State var colors = ["Red", "Green", "Blue"]
-    
+  
     var body: some View {
-        VStack {
-            Group{
-                Button(action: {}) {
-                    Text("Tap here")
+       return  NavigationView{
+            VStack(alignment:.center) {
+                    Group{
+                        Button(action: {}) {
+                            Text("Tap here")
+                        }
+                        
+                        Text("Hacking with Swift")
+                            .background(Color.black)
+                            .foregroundColor(.white)
+                            .padding()
+                            .cornerRadius(12)
+                        
+                        //顺序很重要。。。
+                        Text("Hacking with Swift")
+                            .padding()
+                            .background(Color.black)
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                        
+                        Text("Line")
+                    }
+                
+                    Text("Line")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.red)
+                        .padding()
+                        .background(Color.orange)
+                        .cornerRadius(6)
+                        .padding()
+                        .background(Color.yellow)
+                        .cornerRadius(8)
+                
+                Group{
+                    
+                    Text("Line").padding(.top,15)
+                    
+                    Text("Line").scaleEffect(2)
+                    Text("Line").scaleEffect(x: 1, y: 5)
+                    Text("Line2").scaleEffect(2, anchor: UnitPoint(x: 1, y: 0.5))
+                    Text("Line")
                 }
-                Text("Hacking with Swift")
-                    .background(Color.black)
-                    .foregroundColor(.white)
-                    .padding()
-                    .cornerRadius(12)
-                
-                //顺序很重要。。。
-                Text("Hacking with Swift")
-                    .padding()
-                    .background(Color.black)
-                    .foregroundColor(.white)
-                    .cornerRadius(12)
-                
-                Text("Line")
-            }
+             
+                    Text("Line")
+                    Text("Line")//最多只有10个元素排列
+                    Stepper(onIncrement: {
+                        
+                    }, onDecrement: {
+                        
+                    }) {
+                        Text("Setp")
+                    }
+                   Spacer()
+                }.background(Color.blue)
+                .navigationBarTitle((Text("测试页面12")
+                                        .color(.red)
+                                        .font(.largeTitle)
+                                        .underline()),displayMode: .inline)
         
-            Text("Line")
-                .font(.largeTitle)
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.red)
-                .padding()
-                .background(Color.orange)
-                .cornerRadius(6)
-                .padding()
-                .background(Color.yellow)
-                .cornerRadius(8)
-            
-            
-            Text("Line").padding(.top,15)
-            
-            Text("Line").scaleEffect(2)
-            Text("Line").scaleEffect(x: 1, y: 5)
-            Text("Line2").scaleEffect(2, anchor: UnitPoint(x: 1, y: 0.5))
-            Text("Line") 
-            Text("Line")
-             Text("Line")//最多只有10个元素排列
-            Stepper(onIncrement: {
-                
-            }, onDecrement: {
-                
-            }) {
-                Text("Setp")
-            }
-            
-        }.accentColor(Color.orange)//主题颜色设置
+                //主题颜色设置
+        }.background(Image("happy"))
+        
+         .foregroundColor(Color.pink)
+        
     }
 }
 
--- KBSwiftUI/KBSwiftUI/SceneDelegate.swift
+++ KBSwiftUI/KBSwiftUI/SceneDelegate.swift
@@ -17,10 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let setting = KBUserSettings()
         setting.score = 100
         
-      
+        let navi = UINavigationBar.appearance()
+        navi.barTintColor = UIColor.white //设置导航蓝背景色
+        navi.tintColor = .white
+        
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = UIHostingController(rootView:
-            KBCombineText())
+            ContentView())
         self.window = window
         window.makeKeyAndVisible()
     }
