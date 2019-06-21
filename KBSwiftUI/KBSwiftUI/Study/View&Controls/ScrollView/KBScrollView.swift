a3fa6393ce5f922f3285d392b05f9fc6e9f044dc
金灵波 <lingbo.jin@rokid.com>
2019-06-21 16:09:45 +0800

    [ADD] ScrollView

--- KBSwiftUI/KBSwiftUI/Study/View&Controls/ScrollView/KBScrollView.swift
+++ KBSwiftUI/KBSwiftUI/Study/View&Controls/ScrollView/KBScrollView.swift
@@ -10,23 +10,73 @@ import SwiftUI
 
 struct KBScrollView : View {
     var body: some View {
-        VStack{
+        VStack(spacing:25){
             ScrollView(isScrollEnabled: true, alwaysBounceHorizontal: false, alwaysBounceVertical: false, showsHorizontalIndicator: true, showsVerticalIndicator: true){
                 HStack{
                     ForEach (1...10) { index   in
-                        ZStack(alignment:.bottom){
+                        VStack{
                             Image("happy")
                                 .resizable()
-                                .frame(width: 150, height: 150, alignment: .center)
-                            Text("\(index)").color(.black)
+                                .frame(width: UIScreen.main.bounds.width, height: 100, alignment: .center)
+
+                           Text("\(index)")
+                                .color(.black)
+                                .background(Color.red)
                         }
-                      
-                        
-                    }
+                        }
                 }
-            }.background(Color.yellow)
+            }.frame(height: 150)
+         
+         
+ 
+            Divider()
+            Text("发现Scroll 内容没有填充满整高").color(.gray)
+            Divider()
+            HStack(spacing: 1) {
             
+            GeometryReader { geometry in
+                    ScrollView {
+                        VStack {
+                            ForEach(0..<15) { index in
+                                VStack{
+                                    Image("happy")
+                                        .resizable()
+                                        .frame(width: UIScreen.main.bounds.width, height: 100, alignment: .center)
+                                    Spacer()
+                                    Text("\(index)")
+                                        .color(.black)
+                                        .background(Color.red)
+                                    Spacer()
+                                }
+                            }
+                            }.relativeWidth(1)
+                            .frame(width: geometry.size.width)
+                    }.frame(height: 250)
+                }
+            }
+            
+            Divider()
+            
+            GeometryReader { geometry in
+                ScrollView(isScrollEnabled: true, alwaysBounceHorizontal: false, alwaysBounceVertical: false, showsHorizontalIndicator: true, showsVerticalIndicator: true) {
+                    HStack {
+                        ForEach(0..<15) { index in
+                            VStack{
+                                Image("happy")
+                                    .resizable()
+                                    .frame(width: UIScreen.main.bounds.width, height: 100, alignment: .center)
+                                Spacer()
+                                Text("\(index)")
+                                    .color(.black)
+                                    .background(Color.red)
+                            }
+                        }
+                        }
+                    } .frame(height: 150)
+            }
+ 
         }
+ 
      
     }
 }
