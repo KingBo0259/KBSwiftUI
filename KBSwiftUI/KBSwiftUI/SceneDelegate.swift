3e4b56200f1442bd03e320a3f6d3c3dd3f88c3ae
金灵波 <lingbo.jin@rokid.com>
2019-06-21 11:03:09 +0800

    [ADD] add KBButton

--- KBSwiftUI/KBSwiftUI/SceneDelegate.swift
+++ KBSwiftUI/KBSwiftUI/SceneDelegate.swift
@@ -23,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = UIHostingController(rootView:
-            KBLoadMoreList())
+            KBButton())
         self.window = window
         window.makeKeyAndVisible()
     }
---
+++ KBSwiftUI/KBSwiftUI/Study/View&Controls/Buttons/KBButton.swift
@@ -0,0 +1,113 @@
+//
+//  KBButton.swift
+//  KBSwiftUI
+//
+//  Created by 金灵波 on 2019/6/21.
+//  Copyright © 2019 金灵波. All rights reserved.
+//
+
+import SwiftUI
+
+struct KBButton : View {
+    var body: some View {
+        VStack {
+            Group {
+                Button(action: {
+                    print("click")
+                }){
+                    Text("Simple Button")
+                }
+                Divider()
+                
+                Button(action: {
+                    print("click")
+                }){
+                    Text("shadow Button")
+                    }.padding()
+                    .background(Color.pink)
+                    .cornerRadius(12)
+                    .shadow(color: Color.black, radius: 13, x: 5, y: 5)
+                
+                Divider()
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Image background")
+                    }
+                    .foregroundColor(.red)
+                    .padding().background(Image("happy").resizable())
+                Divider()
+                
+                Button(action: {
+                    
+                }) {
+                    Text("填满整行")
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 40, maxHeight: 40, alignment: .center)
+                    .background(Color.yellow)
+                    .cornerRadius(12)
+                    .padding()
+            }
+            
+            Group{
+                Button(action: {
+                    
+                }) {
+                    HStack{
+                          Image(systemName: "tortoise.fill")
+                         Text("左边图片")
+                    }
+                }.padding()
+                .foregroundColor(.white)
+                .background(Color.purple)
+                
+                
+                Button(action: {
+                    
+                }) {
+                    VStack{
+                        Image(systemName: "hare.fill")
+                        Text("上图下文字")
+                    }
+                    }.padding()
+                    .foregroundColor(.white)
+                    .background(Color.purple)
+                
+                Button(action: {
+                    
+                }) {
+                    HStack{
+                         Text("右图左文字")
+                        Image(systemName: "person.icloud")
+                       
+                    }
+                    }.padding()
+                    .foregroundColor(.white)
+                    .background(Color.purple)
+                
+                Button(action: {
+                    
+                }) {
+                    HStack{
+                        Text("右图做文字")
+                        Image(systemName: "moon.fill")
+                        
+                    }
+                    }.padding()
+                    .foregroundColor(.white)
+                    .background(Color.purple)
+                
+            }
+        }
+       
+    }
+}
+
+#if DEBUG
+struct KBButton_Previews : PreviewProvider {
+    static var previews: some View {
+        KBButton()
+    }
+}
+#endif
