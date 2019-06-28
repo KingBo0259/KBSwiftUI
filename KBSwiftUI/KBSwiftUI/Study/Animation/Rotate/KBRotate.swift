76e248d5df540e75b8a5f385f61068c0e5533a42
金灵波 <lingbo.jin@rokid.com>
2019-06-28 11:58:01 +0800

    [ADD] add animation

--- KBSwiftUI/KBSwiftUI/Study/Animation/Rotate/KBRotate.swift
+++ KBSwiftUI/KBSwiftUI/Study/Animation/Rotate/KBRotate.swift
@@ -10,14 +10,14 @@ import SwiftUI
 
 struct KBRotate : View {
     @State var routation :Double = 0
+    @State var moveX : Length = 0
     
     var body: some View {
         VStack(spacing:50){
             Text("UP we go").rotationEffect(.degrees(-30))
             Text("UP we go").rotationEffect(.degrees(+30))
             Text("UP we go").rotationEffect(.radians(.pi))
-            
-            Text("Slider:\(routation)").rotationEffect(.degrees(routation))
+        Text("Slider:\(routation)").rotationEffect(.degrees(routation))
             
             Slider(value: $routation, from: 0, through: 360, by: 1.0).padding()
             
@@ -27,6 +27,10 @@ struct KBRotate : View {
                 .rotation3DEffect(.degrees(40), axis: (x: 1, y: 0, z: 1))
             
             
+            Text("Move")
+                .offset(x: moveX)
+                .animation(.spring())
+              Slider(value: $moveX, from: -360, through: 360, by: 10.0).padding()
         }
 
 
